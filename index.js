5ebf4c2b716975b441f93582be5849ed531f925f
Dejan Simic <dejansimic@gmx.net>
2022-06-22 20:50:36 +0200

    Color cells on hover

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const container = document.querySelector('.container');
 
+
 for (let i = 1; i <= 16*16; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     container.appendChild(cell);
 }
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'grey';
+    })
+})
